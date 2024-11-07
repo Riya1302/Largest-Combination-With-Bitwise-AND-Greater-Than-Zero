a938d81f023f3faa08745e184ce7031d9c9a7747
Riya Gautam <40192464+Riya1302@users.noreply.github.com>
2024-11-07 22:59:59 +0530

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,11 @@
+var largestCombination = function(candidates) {
+    let ans = 0;
+        for (let i = 0; i < 32; i++) {
+            let cnt = 0;
+            for (const candidate of candidates) {
+                if (candidate & (1 << i)) cnt++;
+            }
+            ans = Math.max(ans, cnt);
+        }
+        return ans;
+};
